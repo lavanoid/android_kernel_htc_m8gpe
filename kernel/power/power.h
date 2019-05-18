e885006ca01ca9996634fbdb35d360b20738bb03
Lava Git <lavanoid@gmail.com>
2019-05-18 13:14:59 +0100

    Add msm-sleeper (screen-off clocks)

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -309,3 +309,7 @@ extern int pm_wake_unlock(const char *buf);
 
 #endif /* !CONFIG_PM_WAKELOCKS */
 
+#ifdef CONFIG_HAS_EARLYSUSPEND2
+/* kernel/power/earlysuspend2.c */
+void request_suspend_state(suspend_state_t state);
+#endif
